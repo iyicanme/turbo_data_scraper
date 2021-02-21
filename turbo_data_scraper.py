700b6d7eebf74a7e8f3ea30fb1a921aeac113aa5
Emre Iyican <git@iyican.me>
2021-02-21 22:02:05 +0300

    Update main function for new worker, Move starting workers to ahead to avoid initialization order problems

--- turbo_data_scraper.py
+++ turbo_data_scraper.py
@@ -1,34 +1,43 @@
+from log.logger import Logger
+from queueing.broker import Broker
+from queueing.dealer import Dealer
 from sink.file_sink import FileSink
 from util.config import Config
-from util.id_dealer import IdDealer
-from util.log import Log
 from worker.details_worker import DetailsWorker
 from worker.history_worker import HistoryWorker
 
 
 def run_turbo_data_scraper():
-    Log()
-    config = Config()
     worker_list = []
+    log_broker = Broker()
+    logger = Logger(log_broker)
+    worker_list.append(logger)
 
-    dealer = IdDealer(config.get_worker_count())
+    config = Config()
+
+    id_dealer = Dealer()
 
     worker = HistoryWorker(config.get_api_key(),
                            config.get_sleep_duration(),
-                           sink=dealer,
+                           log_broker.create_queue(),
+                           sink=id_dealer,
                            start_match_id=config.get_start_match_id(),
-                           matches_requested=config.get_matches_requested()).start()
+                           matches_requested=config.get_matches_requested())
     worker_list.append(worker)
 
     for i in range(config.get_worker_count()):
         worker = DetailsWorker(config.get_api_key(),
                                config.get_sleep_duration(),
-                               source=dealer.get_queue(i),
+                               log_broker.create_queue(),
+                               source=id_dealer.create_queue(),
                                sink=FileSink(path=config.get_data_path(),
                                              file_name_pattern=config.get_file_name_pattern(),
                                              unique_id=config.get_worker_name_pattern().format(i + 1),
-                                             max_size=pow(2, config.get_file_size_binary_power()))).start()
+                                             max_size=pow(2, config.get_file_size_binary_power())))
         worker_list.append(worker)
+
+    for worker in worker_list:
+        worker.start()
 
     for worker in worker_list:
         worker.join()
