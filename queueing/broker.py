8735a8bfe5718af3a35c0a0e1f27aa9ad49a41cd
Emre Iyican <git@iyican.me>
2021-02-21 21:52:30 +0300

    Add broker for logging

---
+++ queueing/broker.py
@@ -0,0 +1,25 @@
+from queueing.queue import Queue
+
+
+class Broker:
+    def __init__(self):
+        self.queue_count = 0
+        self.queues = []
+
+        self.sequence = 0
+
+    def create_queue(self):
+        queue = Queue()
+
+        self.queues.append(queue)
+        self.queue_count += 1
+
+        return queue
+
+    def dequeue(self):
+        queue_index = self.sequence
+
+        self.sequence += 1
+        self.sequence %= self.queue_count
+
+        return self.queues[queue_index].dequeue()
