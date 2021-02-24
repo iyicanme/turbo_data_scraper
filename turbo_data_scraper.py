9c24c4a6fcc6f5a577919090e0f79e20ab96fdec
Emre Iyican <git@iyican.me>
2021-02-25 01:59:42 +0300

    Restructure main program logic, Add creating config.json if it does not exist, Remove config.json

--- turbo_data_scraper.py
+++ turbo_data_scraper.py
@@ -1,47 +1,88 @@
 from log.logger import Logger
-from queueing.broker import Broker
-from queueing.dealer import Dealer
+from queuing.broker import Broker
+from queuing.dealer import Dealer
 from sink.file_sink import FileSink
 from util.config import Config
 from worker.details_worker import DetailsWorker
 from worker.history_worker import HistoryWorker
 
 
-def run_turbo_data_scraper():
-    worker_list = []
-    log_broker = Broker()
-    logger = Logger(log_broker)
-    worker_list.append(logger)
+class TurboDataScraper:
+    def __init__(self):
+        if not TurboDataScraper.config_exists():
+            TurboDataScraper.create_empty_config_file()
+            exit(1)
 
-    config = Config()
+        self.log_broker = Broker()
+        self.logger = Logger(self.log_broker)
 
-    id_dealer = Dealer()
+        self.config = Config()
 
-    worker = HistoryWorker(config.get_api_key(),
-                           config.get_sleep_duration(),
-                           log_broker.create_queue(),
-                           sink=id_dealer,
-                           start_match_id=config.get_start_match_id(),
-                           matches_requested=config.get_matches_requested())
-    worker_list.append(worker)
+        self.id_dealer = Dealer()
+        self.history_worker = HistoryWorker(self.config.get_api_key(),
+                                            self.config.get_sleep_duration(),
+                                            self.log_broker.create_queue(),
+                                            sink=self.id_dealer,
+                                            start_match_id=self.config.get_start_match_id(),
+                                            matches_requested=self.config.get_matches_requested())
 
-    for i in range(config.get_worker_count()):
-        worker = DetailsWorker(config.get_api_key(),
-                               config.get_sleep_duration(),
-                               log_broker.create_queue(),
-                               source=id_dealer.create_queue(),
-                               sink=FileSink(path=config.get_data_path(),
-                                             file_name_pattern=config.get_file_name_pattern(),
-                                             unique_id=config.get_worker_name_pattern().format(i + 1),
-                                             max_size=pow(2, config.get_file_size_binary_power())))
-        worker_list.append(worker)
+        self.workers = []
+        for i in range(self.config.get_worker_count()):
+            worker = DetailsWorker(self.config.get_api_key(),
+                                   self.config.get_sleep_duration(),
+                                   self.log_broker.create_queue(),
+                                   source=self.id_dealer.create_queue(),
+                                   sink=FileSink(path=self.config.get_data_path(),
+                                                 file_name_pattern=self.config.get_file_name_pattern(),
+                                                 unique_id=self.config.get_worker_name_pattern().format(i + 1),
+                                                 max_size=pow(2, self.config.get_file_size_binary_power())))
+            self.workers.append(worker)
 
-    for worker in worker_list:
-        worker.start()
+    def start(self):
+        self.logger.start()
 
-    for worker in worker_list:
-        worker.join()
+        self.history_worker.start()
+
+        for worker in self.workers:
+            worker.start()
+
+        return self
+
+    def join(self):
+        for worker in self.workers:
+            worker.join()
+
+        self.history_worker.join()
+
+        self.logger.join()
+
+    @staticmethod
+    def config_exists():
+        from os.path import exists
+
+        return exists("config.json")
+
+    @staticmethod
+    def create_empty_config_file():
+        if TurboDataScraper.config_exists():
+            return
+
+        config = {
+            "api_key": "",
+            "start_match_id": 0,
+            "matches_requested": 0,
+            "data_path": "",
+            "file_name_pattern": "",
+            "worker_name_pattern": "",
+            "worker_count": 0,
+            "file_size_binary_power": 0,
+            "sleep_duration": 0
+        }
+
+        from json import dump
+        with open("config.json", "w") as config_file:
+            dump(config, config_file)
 
 
 if __name__ == '__main__':
-    run_turbo_data_scraper()
+    TurboDataScraper().start().join()
